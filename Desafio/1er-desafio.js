65b552cd750bb3818f9e1e2cd351b8c0af6c958e
usuario <jqncaraballo@gmail.com>
2023-08-07 13:28:50 -0300

    first commit

---
+++ Desafio/1er-desafio.js
@@ -0,0 +1,14 @@
+class ProductManager{
+    constructor() {
+        this.products = []
+}
+
+    addProduct(price, description, title, code, stock, thumbnail){
+        price: price
+        description: description
+        title: title
+        code: code
+        stock: stock
+        thumbnail: thumbnail
+    }
+}   
