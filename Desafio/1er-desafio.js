45a1385c415733cb3fe07620121e35d5c1cad366
usuario <jqncaraballo@gmail.com>
2023-08-11 17:40:11 -0300

    desafio 1, completado

--- Desafio/1er-desafio.js
+++ Desafio/1er-desafio.js
@@ -1,37 +1,60 @@
-class ProductManager{
+class ProductManager {
     constructor() {
-        this.products = []
-}
+        this.Products = []
+        this.nextProductId = 1 // Utilizar una variable para generar IDs únicos
+    }
 
-    addProduct(price, description, title, code, stock, thumbnail){
-        const product = {
-            price: price,
-            description: description,
-            title: title,
-            code: code,
-            stock: stock,
-            thumbnail: thumbnail
+    addProduct(title, description, price, thumbnail, stock) {
+        const code = this.nextProductId.toString().padStart(4, '0')
+
+        const productExists = this.Products.some(
+            product => product.title === title && product.code === code
+        )
+
+        if (productExists) {
+            throw new Error(`Error: Ya existe un producto con el mismo título y código.`)
         }
-        this.products.push(product)
-    }   
+
+        let nuevoProducto = {
+            id: this.nextProductId, // Usar el ID único generado
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        this.Products.push(nuevoProducto)
+        this.nextProductId++ // Incrementar el ID único
+    }
+
     getProducts() {
-        return this.products
+        return this.Products
     }
+
     getProductById(id) {
-        const product = this.products.find((prod) => prod.code === id)
-        if (product) {
-            return product
-        } else {
-            console.log("Not found")
+        const product = this.Products.find(product => product.id === id)
+        if (!product) {
+            throw new Error(`Error: No se encontró ningún producto con el ID ${id}.`)
         }
+        return product
     }
 }
-const manager = new ProductManager()
-manager.addProduct(10, "color azul", "casa", "01", 3, "imagen1.jpg")
-manager.addProduct(20, "color gris", "edificio", "02", 2, "imagen2.jpg")
 
-//console.log(manager.getProducts()) // Si yo quito las "//" se mostrarán los productos
+let manager = new ProductManager()
 
-const producto1 = manager.getProductById("01")
+try {
+    manager.addProduct('mesa', 'cuadrada', 500, 'foto', 123, 500)
+    manager.addProduct('mate', 'redondo', 200, 'foto', 321, 300)
+    manager.addProduct('notebook', 'roja', 9500, 'foto', 323, 100)
+    manager.addProduct('notebook', 'roja', 9500, 'foto', 323, 100)
 
-const producto2 = manager.getProductById("03")
+
+    const product = manager.getProductById(2)
+    console.log("Producto encontrado:", product)
+
+    const nonExistentProduct = manager.getProductById(5);
+} catch (error) {
+    console.error(error.message)
+}
