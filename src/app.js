eeffd7cdfe99dbcbf0296822fe53b804a9540497
usuario <jqncaraballo@gmail.com>
2023-09-05 09:55:30 -0300

    aplicado el puerto 3000 funcional

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express=require('express')
+const http = require('http')
+const socketIo = require('socket.io')
 const productsRouter=require('../routes/products.router')
 const cartsRouter=require('../routes/cart.router')
 const handlebars = require( 'express-handlebars')
@@ -6,16 +8,22 @@ const PORT=3000
 
 const app=express()
 
+const server = http.createServer(app) // Crea el servidor HTTP con Express.
+
+// Configura Socket.IO en el servidor HTTP.
+const io = socketIo(server)
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 
 
 //app.use('/api/products',productsRouter)
 //app.use('/api/carts',cartsRouter)
+//pp.use('/realtimeproducts',)
 
 app.get('/', (req, res) => {
     
@@ -24,13 +32,12 @@ app.get('/', (req, res) => {
     res.status(200).render('home', {
         nombre,
         titulo: 'home page'
-    });
-    
-    
-});
+    })
+})
 
 
 
-const server=app.listen(PORT,()=>{
+
+server.listen(PORT, () => {
     console.log(`Server escuchando en puerto ${PORT}`)
 })
